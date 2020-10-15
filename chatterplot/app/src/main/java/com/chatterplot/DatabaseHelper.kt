01d871ebe019f654a7a22846ed4b545c31652e5b
sungrae <sungrae@umich.edu>
2020-10-15 15:17:10 -0400

    sql

--- chatterplot/app/src/main/java/com/chatterplot/DatabaseHelper.kt
+++ chatterplot/app/src/main/java/com/chatterplot/DatabaseHelper.kt
@@ -1,5 +1,6 @@
 package com.chatterplot
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
@@ -25,14 +26,24 @@ class DatabaseHelper(val context: Context) : SQLiteOpenHelper(context, DATABASE_
         onCreate(db)
     }
 
-    fun getDB(): SQLiteDatabase {
-        return this.writableDatabase
+    fun insertRow(tableName:String, values:ContentValues) {
+        val db = this.writableDatabase
+        db.insertOrThrow(tableName, null, values)
+    }
+
+    fun deleteRow(tableName:String, where:String?, args:Array<String>?) {
+        val db = this.writableDatabase
+        db.delete(tableName, where, args)
+    }
+
+    fun updateRow(tableName:String, values:ContentValues, where:String?, args: Array<String>?) {
+        this.writableDatabase.update(tableName, values, where, args)
     }
 
     fun createTable(schema: Schema) {
         try {
             val db = this.writableDatabase
-            
+
             var sqlQuery = "CREATE TABLE ${schema.name} (" +
                     "ID INTEGER PRIMARY KEY,"
             for (idx in 0 until schema.columns.size) {

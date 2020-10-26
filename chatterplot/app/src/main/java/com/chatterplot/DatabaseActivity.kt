5d50e8a9ae3c5bad037c7d4c999288b21068215e
sungrae <sungrae@umich.edu>
2020-10-26 19:52:36 -0400

    Added back button to database activity

--- chatterplot/app/src/main/java/com/chatterplot/DatabaseActivity.kt
+++ chatterplot/app/src/main/java/com/chatterplot/DatabaseActivity.kt
@@ -3,13 +3,17 @@ package com.chatterplot
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.NavUtils
 
 class DatabaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_database)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
         getListOfTables(findViewById<TextView>(R.id.dbList))
     }
 
@@ -20,5 +24,14 @@ class DatabaseActivity : AppCompatActivity() {
         val info = db.getTable("test5")
 //        text.text = dbList.joinToString("\n")
         text.text = info.toString()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            android.R.id.home-> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

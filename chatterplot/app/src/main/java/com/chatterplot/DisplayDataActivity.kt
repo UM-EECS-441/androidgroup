060e92b90795865fd460ab180c4d38a352c86f40
benberk <benberk@umich.edu>
2020-10-27 11:04:02 -0400

    tablelayout activity w custom adapterto display one column numerical data

--- chatterplot/app/src/main/java/com/chatterplot/DisplayDataActivity.kt
+++ chatterplot/app/src/main/java/com/chatterplot/DisplayDataActivity.kt
@@ -3,11 +3,17 @@ package com.chatterplot
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class DisplayDataActivity : AppCompatActivity() {
-    var loader: TableLoader? = null
+    lateinit var adapter: TableAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_data)
+
+        //create tableAdapter
+        adapter = TableAdapter(this, "", "")
+        adapter.loadTable()
+        //intent should include DB path/name and specified DB to open
     }
 }

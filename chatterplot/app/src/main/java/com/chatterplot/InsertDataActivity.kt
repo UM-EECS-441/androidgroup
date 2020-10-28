db249ffb4c8e6d97ed5e987670f0664c634483a7
sungrae <sungrae@umich.edu>
2020-10-28 11:01:31 -0400

    Inserting now looks at databbase to determine label and number of columns

--- chatterplot/app/src/main/java/com/chatterplot/InsertDataActivity.kt
+++ chatterplot/app/src/main/java/com/chatterplot/InsertDataActivity.kt
@@ -3,18 +3,20 @@ package com.chatterplot
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import com.google.android.material.textfield.TextInputLayout
 
 class InsertDataActivity : AppCompatActivity() {
-
-
-    lateinit var xVal : EditText
-    lateinit var yVal : EditText
     lateinit var confirmButton: Button
-
+    private var columns: ArrayList<View> = ArrayList<View>()
     private lateinit var datasetName : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,17 +24,45 @@ class InsertDataActivity : AppCompatActivity() {
         setContentView(R.layout.activity_insert_data)
 
         datasetName = intent.getStringExtra("DATASETNAME")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = datasetName
 
         val titleText : TextView = findViewById<TextView>(R.id.InsertDatasetName)
         val preString : String = "Insert Into "
         titleText.text = preString + datasetName
 
-        xVal = findViewById(R.id.xInput)
-        yVal = findViewById(R.id.yInput)
+        val columnNames = DatabaseHelper(this).getColumnNames(datasetName)
+
+        val parent = findViewById<ConstraintLayout>(R.id.insert_data)
+        val setter = ConstraintSet()
+        for(idx in 1 until columnNames.size-1) {
+            val name = columnNames[idx]
+            val inflater = LayoutInflater.from(this)
+            val input = inflater.inflate(R.layout.column_insert_field, null, false)
+            input.findViewById<TextInputLayout>(R.id.column_input).hint = name
+            input.id = View.generateViewId()
+            parent.addView(input)
+            setter.clone(parent)
+            if(columns.size == 0) {
+                setter.connect(input.id, ConstraintSet.TOP, titleText.id, ConstraintSet.BOTTOM)
+            }
+            else {
+                setter.connect(input.id, ConstraintSet.TOP, columns.last().id, ConstraintSet.BOTTOM)
+            }
+            setter.applyTo(parent)
+            columns.add(input)
+        }
+
+
+
+//        xVal = findViewById(R.id.xInput)
+//        yVal = findViewById(R.id.yInput)
         confirmButton = findViewById(R.id.confirmInsertButton)
 
         confirmButton.setOnClickListener {
-            insertFunction(arrayListOf(xVal.text.toString(), yVal.text.toString()))
+            insertFunction(ArrayList(columns.map{
+                it.findViewById<TextInputLayout>(R.id.column_input).editText?.text.toString()
+            }))
         }
     }
 
@@ -42,5 +72,14 @@ class InsertDataActivity : AppCompatActivity() {
         t.show()
         val intent = Intent(this, MainActivity::class.java)
         finish()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            android.R.id.home-> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
